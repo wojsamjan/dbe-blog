a4969e42cd6529b0dc0a343b8522f5ad8945b304
Wojciech Januszek <wojsamjan@gmail.com>
2018-02-09 00:54:37 +0100

    add post_detail view

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,14 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
