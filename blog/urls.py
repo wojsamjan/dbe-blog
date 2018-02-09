c1fb01a20dd95aa3613eff0f53e5643f861bfba8
Wojciech Januszek <wojsamjan@gmail.com>
2018-02-10 00:43:41 +0100

    update blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # post views
     # url(r'^$', views.post_list, name='post_list'),
     url(r'^$', views.PostListView.as_view(), name='post_list'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'
         r'(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
