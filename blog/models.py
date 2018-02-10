e8aaf2a243b68e31e291d3bb99a1f79872c25595
Wojciech Januszek <wojsamjan@gmail.com>
2018-02-10 16:39:04 +0100

    add tag manager to Post model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -27,14 +28,15 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+    tags = TaggableManager()
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
-
-    objects = models.Manager()  # The default manager.
-    published = PublishedManager()  # Our custom manager.
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
