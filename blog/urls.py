dc47d9b41b6f7719dd1a47251a44b1ed482e981d
Wojciech Januszek <wojsamjan@gmail.com>
2018-02-12 22:50:18 +0100

    register RSS URL

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     # post views
@@ -13,4 +15,5 @@ urlpatterns = [
         name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share,
         name='post_share'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
